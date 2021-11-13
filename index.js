77ba97f953fa689ef409a930312bb204d67e850c
Shubham Jha <shubham.jha2001m@gmail.com>
2021-11-13 19:16:16 +0530

    mongodb connection

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.listen(5000, () => {
-    console.log(`Server Running`)
-}) 
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+mongoose.connect(process.env.MONGODB_URI, 
+        { useNewUrlParser: true, useUnifiedTopology: true },
+    )
+    .then(() => console.log("MongoDB Connected ..."))
+    .catch((err) => console.log(err));
+
+app.listen(port || 5000, () => {
+    console.log(`Server Running on Port ${port}`)
+}); 
