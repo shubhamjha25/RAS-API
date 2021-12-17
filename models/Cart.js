a6a8afc2a6caee792e404e0cb2300b190faae0e6
Shubham Jha <shubham.jha2001m@gmail.com>
2021-12-17 21:33:36 +0530

    cart model

---
+++ models/Cart.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const CartSchema = new mongoose.Schema(
+  {
+    userId: { 
+        type: String, 
+        required: true 
+    },
+    items: [
+      {
+        itemId: {
+            type: String,
+        },
+        quantity: {
+            type: Number,
+            default: 1,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Cart", CartSchema);
