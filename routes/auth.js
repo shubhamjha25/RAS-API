69d7eb886b46a6adc33352ccb2cf03b56d076c31
Shubham Jha <shubham.jha2001m@gmail.com>
2021-12-18 21:58:43 +0530

    jwt implementation

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const CryptoJS = require("crypto-js");
+const jwt = require('jsonwebtoken');
 
 // REGISTER USER
 router.post("/register", async (req, res) => {
@@ -34,9 +35,17 @@ router.post("/login", async (req, res) => {
             return res.status(400).json("Incorrect Password");
         }
 
+        const accessToken = jwt.sign({
+                id: user._id,
+                isAdmin: user.isAdmin
+            },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: "1d" }
+        );
+
         const { password, ...others } = user._doc;
 
-        res.status(200).json(others);
+        res.status(200).json({ ...others, accessToken });
     } catch (err) {
         res.status(500).json(err);
     }
