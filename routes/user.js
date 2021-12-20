09243352206a12a91a6be9fe2a5b3d15aef2d85a
Shubham Jha <shubham.jha2001m@gmail.com>
2021-12-20 21:22:07 +0530

    core: get all users

--- routes/user.js
+++ routes/user.js
@@ -39,4 +39,17 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
     }
 });
 
+// GET ALL USERS (ONLY ADMIN)
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    const query = req.query.new;
+    try {
+        const users = query
+            ? await User.find().sort({ _id: -1 })
+            : await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
