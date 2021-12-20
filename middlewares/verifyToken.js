fc7e52fab0e1ea79663935ccc3b2ae42e128e45c
Shubham Jha <shubham.jha2001m@gmail.com>
2021-12-20 20:52:23 +0530

    auth: verify token & authorize admin

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -24,4 +24,14 @@ const verifyTokenAndAuthorization = (req, res, next) => {
     });
 };
 
-module.exports = { verifyToken, verifyTokenAndAuthorization };
+const verifyTokenAndAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if(req.user.isAdmin) {
+            next();
+        } else {
+            res.status(403).json("You Are Not Authorized");
+        }
+    });
+};
+
+module.exports = { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin };
