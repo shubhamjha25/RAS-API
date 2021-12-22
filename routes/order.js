7d99dc1f6dd29e7e05736840d48137981351bc0d
Shubham Jha <shubham.jha2001m@gmail.com>
2021-12-22 23:05:33 +0530

    feat: create an order

---
+++ routes/order.js
@@ -0,0 +1,18 @@
+const Order = require("../models/Order");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
+
+const router = require("express").Router();
+
+// CREATE AN ORDER
+router.post("/", verifyToken, async (req, res) => {
+    const newOrder = new Order(req.body);
+
+    try {
+        const savedOrder = await newOrder.save();
+        res.status(200).json(savedOrder);
+    } catch (err) {
+        res.status(500).json(err);
+  }
+});
+
+module.exports = router;
