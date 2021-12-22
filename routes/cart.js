c25f81158baac1f40d59294d232fc5e338e09aa3
Shubham Jha <shubham.jha2001m@gmail.com>
2021-12-22 22:53:32 +0530

    feat: delete cart

--- routes/cart.js
+++ routes/cart.js
@@ -31,4 +31,14 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     }
 });
 
+// DELETE CART
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        await Cart.findByIdAndDelete(req.params.id);
+        res.status(200).json("Cart Has Been Deleted");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
