9294ea75bc94340ab88b074c23c4efef9522834e
Shubham Jha <shubham.jha2001m@gmail.com>
2021-12-22 22:54:51 +0530

    core: get all carts

--- routes/cart.js
+++ routes/cart.js
@@ -51,4 +51,14 @@ router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
     }
 });
 
+// GET ALL CARTS (FOR ADMIN)
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const carts = await Cart.find();
+        res.status(200).json(carts);
+    } catch (err) {
+        res.status(500).json(err);
+  }
+});
+
 module.exports = router;
