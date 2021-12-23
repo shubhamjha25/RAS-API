467da3563ab5fc3700288bc4263400b1a809a65c
Shubham Jha <shubham.jha2001m@gmail.com>
2021-12-23 16:58:50 +0530

    feat: stripe payments integration

---
+++ routes/stripe.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+
+const stripe_key = process.env.STRIPE_SECRET_KEY;
+const stripe = require('stripe')(stripe_key);
+
+router.post("/payment", (req, res) => {
+    stripe.charges.create(
+        {
+            source: req.body.tokenId,
+            amount: req.body.amount,
+            currency: "inr",
+        },
+        (stripeErr, stripeRes) => {
+            if (stripeErr) {
+                res.status(500).json(stripeErr);
+            } else {
+                res.status(200).json(stripeRes);
+            }
+        }
+    );
+});
+
+module.exports = router;
