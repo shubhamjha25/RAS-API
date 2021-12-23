a660f6014cd01054639e0cf062a74b0121b7a1ed
Shubham Jha <shubham.jha2001m@gmail.com>
2021-12-23 14:47:31 +0530

    core: get all user orders

--- routes/order.js
+++ routes/order.js
@@ -51,4 +51,14 @@ router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
+// GET ALL ORDERS (FOR ADMIN)
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const orders = await Order.find();
+        res.status(200).json(orders);
+    } catch (err) {
+        res.status(500).json(err);
+  }
+});
+
 module.exports = router;
