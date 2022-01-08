ebdc5c39f156d0d9833b825907000ce21b46ae38
Shubham Jha <shubham.jha2001m@gmail.com>
2022-01-08 18:30:53 +0530

    fix: order amount issue

--- models/Cart.js
+++ models/Cart.js
@@ -21,6 +21,9 @@ const CartSchema = new mongoose.Schema(
             type: Number,
             default: 1,
         },
+        price: {
+            type: Number
+        }
       },
     ],
   },
