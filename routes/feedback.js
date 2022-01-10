5d7ecc5c28d245810273997e460cd3a46c7d9b65
Shubham Jha <shubham.jha2001m@gmail.com>
2022-01-10 23:00:08 +0530

    feat: post feedback

--- routes/feedback.js
+++ routes/feedback.js
@@ -3,6 +3,16 @@ const { verifyToken, verifyTokenAndAdmin } = require("../middlewares/verifyToken
 
 const router = require("express").Router();
 
-// Endpoints
+// SEND A FEEDBACK
+router.post("/new", verifyToken, async (req,res) => {
+    const newFeedback = new Feedback(req.body);
+
+    try {
+        const savedFeedback = await newFeedback.save();
+        res.status(200).json(savedFeedback);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
